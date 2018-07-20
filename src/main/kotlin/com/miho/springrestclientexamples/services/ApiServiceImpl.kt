ba6323b6544090a12716a06f232fcacf75264b26
Michael Hornegger <michael.hornegger@karriere.at>
2018-07-20 12:00:40 +0200

    Externalize api url as an application property

--- src/main/kotlin/com/miho/springrestclientexamples/services/ApiServiceImpl.kt
+++ src/main/kotlin/com/miho/springrestclientexamples/services/ApiServiceImpl.kt
@@ -2,14 +2,20 @@ package com.miho.springrestclientexamples.services
 
 import com.miho.api.domain.User
 import com.miho.api.domain.UserData
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.util.UriComponentsBuilder
 
 @Service
-class ApiServiceImpl(private val restTemplate: RestTemplate) : ApiService {
+class ApiServiceImpl(private val restTemplate: RestTemplate, @Value("\${api.url}") private val apiUrl: String) : ApiService {
 
     //for whatever reason, the api always returns one additional data record
-    override fun getUsers(limit: Int): List<User> =
-            restTemplate.getForObject("http://apifaketory.com/api/user?limit=$limit", UserData::class.java)?.data
-                    ?: throw RuntimeException("it all went to shit")
+    override fun getUsers(limit: Int): List<User> {
+        val uriComponentsBuilder = UriComponentsBuilder.fromUriString(apiUrl)
+                .queryParam("limit", limit)
+
+        return restTemplate.getForObject(uriComponentsBuilder.toUriString(), UserData::class.java)?.data
+                ?: throw RuntimeException("it all went to shit")
+    }
 }
