c3df4c42aba8fecfbdd49d4a6a95f870cc34afcb
Michael Hornegger <michael.hornegger@karriere.at>
2018-07-20 15:22:44 +0200

    Add a reactive method to get all users

--- src/main/kotlin/com/miho/springrestclientexamples/services/ApiService.kt
+++ src/main/kotlin/com/miho/springrestclientexamples/services/ApiService.kt
@@ -1,8 +1,12 @@
 package com.miho.springrestclientexamples.services
 
 import com.miho.api.domain.User
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 interface ApiService {
 
     fun getUsers(limit: Int): List<User>
+
+    fun getUsers(limit: Mono<Int>): Flux<User>
 }
--- src/main/kotlin/com/miho/springrestclientexamples/services/ApiServiceImpl.kt
+++ src/main/kotlin/com/miho/springrestclientexamples/services/ApiServiceImpl.kt
@@ -3,14 +3,17 @@ package com.miho.springrestclientexamples.services
 import com.miho.api.domain.User
 import com.miho.api.domain.UserData
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.util.UriComponentsBuilder
+import reactor.core.publisher.Mono
 
 @Service
+//for whatever reason, the api always returns one additional data record
 class ApiServiceImpl(private val restTemplate: RestTemplate, @Value("\${api.url}") private val apiUrl: String) : ApiService {
 
-    //for whatever reason, the api always returns one additional data record
     override fun getUsers(limit: Int): List<User> {
         val uriComponentsBuilder = UriComponentsBuilder.fromUriString(apiUrl)
                 .queryParam("limit", limit)
@@ -18,4 +21,12 @@ class ApiServiceImpl(private val restTemplate: RestTemplate, @Value("\${api.url}
         return restTemplate.getForObject(uriComponentsBuilder.toUriString(), UserData::class.java)?.data
                 ?: throw RuntimeException("it all went to shit")
     }
+
+    override fun getUsers(limit: Mono<Int>) = WebClient.create(apiUrl)
+            .get()
+            .uri { it.queryParam("limit", limit.block()).build() }
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .flatMap { it.bodyToMono(UserData::class.java) }
+            .flatMapIterable(UserData::data)
 }
